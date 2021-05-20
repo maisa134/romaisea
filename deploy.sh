288d5d6ccd8f47b9d755bac08b532ad614c005d9
maisa134 <73913853+maisa134@users.noreply.github.com>
2021-05-20 20:13:46 +0100

    adding deploy files

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:maisa134/romaisea.git main:gh-pages
+git push -f git@github.com:maisa134/romaisea.git master:gh-pages
 
 cd -
